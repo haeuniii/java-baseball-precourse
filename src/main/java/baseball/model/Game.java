4550416a598ee96f77d6d4b4618688ef4a70976b
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-04 22:46:27 +0900

    [ADD] ball 개수 계산 기능 추가
    ball 개수 계산 기능 추가

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -1,15 +1,19 @@
 package baseball.model;
 
+import baseball.utils.StartCode;
+
 public class Game {
     private int source;
     private int target;
     private int strike;
+    private int ball;
 
     public Game(String _source, int _target) {
         this.source = Integer.valueOf(_source);
         this.target = _target;
 
         setStrike();
+        setBall();
     }
 
     public void setStrike() {
@@ -28,5 +32,22 @@ public class Game {
 
     public int getStrikeCount() {
         return strike;
+    }
+
+    public int getBallCount() {
+        return ball;
+    }
+
+    public void setBall() {
+        String _source = String.valueOf(this.source);
+        int digit = _source.length();
+
+        for (String s : String.valueOf(this.target).split("")) {
+            if (_source.contains(s)) {
+                _source = _source.replaceFirst(s, "");
+            }
+        }
+
+        this.ball = digit - _source.length() - this.strike;
     }
 }
