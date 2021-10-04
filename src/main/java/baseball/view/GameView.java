7fd6c4a95a3c7590eae9312cf5997727609c04a5
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-04 22:56:03 +0900

    [ADD] ball, strike, nothing 결과 출력 기능 추가
    ball, strike, nothing 결과 출력 기능 추가

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -7,7 +7,19 @@ public class GameView {
         System.out.print(Message.START.getMessage());
     }
 
-    public static void printGameReStart() {
+    public static void printGameRestart() {
         System.out.print(Message.RESTART.getMessage());
+    }
+
+    public static void printStrike(int strikeCount) {
+        if (strikeCount > 0)    System.out.println(strikeCount + Message.STRIKE.getMessage());
+    }
+
+    public static void printBall(int ballCount) {
+        if (ballCount > 0)      System.out.println(ballCount + Message.BALL.getMessage());
+    }
+
+    public static void printNothing(boolean isNothing) {
+        if (isNothing)  System.out.println(Message.NOTHING.getMessage());
     }
 }
