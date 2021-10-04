3b71a1e9c88e96e1d090021f67e2a90a6faf18be
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-04 23:03:57 +0900

    [ADD] Controller class 화면 출력 기능 추가
    랜덤 수 추출 및 결과를 화면에 출력하는 기능 추가(Controller class)

--- src/main/java/baseball/controller/GameProcessor.java
+++ src/main/java/baseball/controller/GameProcessor.java
@@ -1,6 +1,9 @@
 package baseball.controller;
 
+import baseball.model.Game;
 import baseball.utils.NumberRange;
+import baseball.view.GameView;
+import nextstep.utils.Console;
 import nextstep.utils.Randoms;
 
 public class GameProcessor {
@@ -8,5 +11,17 @@ public class GameProcessor {
         boolean isContinue = true;
         int targetNumber = Randoms.pickNumberInRange(NumberRange.START.getRange(), NumberRange.END.getRange());
 
+        while (isContinue) {
+            GameView.printGameStart();
+            Game game = new Game(Console.readLine(), targetNumber);
+
+            GameView.printStrike(game.getStrikeCount());
+            GameView.printBall(game.getBallCount());
+            GameView.printNothing(!game.isRight() && game.isNothing());
+            GameView.printSuccess(game.isRight());
+
+            isContinue = game.isRight() ? game.isGameContinue(Console.readLine()) : true;
+            targetNumber = isContinue == true && game.isRight() ? Randoms.pickNumberInRange(NumberRange.START.getRange(), NumberRange.END.getRange()) : targetNumber;
+        }
     }
 }
