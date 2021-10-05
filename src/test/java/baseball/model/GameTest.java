b15c34d73926a046d635a0513263cfe3e5561f6d
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-05 23:17:59 +0900

    [ADD] model 단위 테스트에 대한 테스트 클래스 추가
     model 단위 테스트에 대한 junit 테스트 클래스 추가

--- src/test/java/baseball/model/GameTest.java
+++ src/test/java/baseball/model/GameTest.java
@@ -7,20 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
 
     @Test
-    void setStrike() {
-        Game game = new Game("222", 123);
-    }
-
-    @Test
     void getStrikeCount() {
     }
 
     @Test
     void getBallCount() {
-    }
-
-    @Test
-    void setBall() {
     }
 
     @Test
