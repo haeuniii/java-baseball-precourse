c03b56f920c1f427e66a079cd6e6483a04e92109
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-05 23:14:28 +0900

    [UPD] 화면 출력 메소드에 줄바꿈 기능 추가
    스크라이크, 볼 출력 기능에 줄 바꿈 기능 추가

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,37 @@
+package baseball.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void setStrike() {
+        Game game = new Game("222", 123);
+    }
+
+    @Test
+    void getStrikeCount() {
+    }
+
+    @Test
+    void getBallCount() {
+    }
+
+    @Test
+    void setBall() {
+    }
+
+    @Test
+    void isNothing() {
+    }
+
+    @Test
+    void isRight() {
+    }
+
+    @Test
+    void isGameContinue() {
+    }
+}
