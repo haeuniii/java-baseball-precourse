0a8dcdfad7b314bcb9df6a9eb790223ac7c74aa5
haeuniii <qnfnqnfn9825@gmail.com>
2021-10-06 22:30:50 +0900

    [UPD] 모델 단위 기능 테스트 추가
    모델 단위기능 테스트 추가

--- src/test/java/baseball/model/GameTest.java
+++ src/test/java/baseball/model/GameTest.java
@@ -1,28 +1,40 @@
 package baseball.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameTest {
+    private static Game game;
+
+    @BeforeAll
+    static void initAll() {
+        game = new Game("226", 123);
+    }
 
     @Test
     void getStrikeCount() {
+        Assertions.assertEquals(1, game.getStrikeCount());
     }
 
     @Test
     void getBallCount() {
+        Assertions.assertEquals(0, game.getBallCount());
     }
 
     @Test
     void isNothing() {
+        Assertions.assertFalse(game.isNothing());
     }
 
     @Test
     void isRight() {
+        Assertions.assertFalse(game.isRight());
     }
 
     @Test
     void isGameContinue() {
+        Assertions.assertTrue(game.isGameContinue("1"));
+        Assertions.assertFalse(game.isGameContinue("2"));
     }
 }
